6dabd2e37cce6620433e73c5656b42b284db901f
Joseph Ditton <jditton.atomic@gmail.com>
2021-01-25 09:37:19 -0700

    adds first project

---
+++ JavaIntro/src/Assignment0.java
@@ -0,0 +1,7 @@
+
+public class Assignment0
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ JavaIntro/src/OtherProgram.java
@@ -0,0 +1,5 @@
+public class OtherProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello from program 2.");
+    }
+}
